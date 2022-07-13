8eca28735bd35a2f2636d3d738761e36d7174a0c
DESKTOP-Q1VIMM1\keven <kevenescovedo1916@gmail.com>
2022-07-13 08:43:18 -0300

    first commit in java ide dio

---
+++ src/br/com/escovedo/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.escovedo;
+
+import br.com.escovedo.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/*int a = 7;
+		int b = 7;
+		System.out.println("Olá MUNDO!!! keven " + (a + b)); */
+		Gato gato = new Gato();
+		System.out.println(gato);
+		
+	}
+}
+
